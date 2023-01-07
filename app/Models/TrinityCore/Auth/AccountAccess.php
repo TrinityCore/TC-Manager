2672da5cc5ac3ec997cc66c89adbb8e6ed3bb41e
Chris Deaton <dcdeaton@ncsu.edu>
2023-01-07 18:04:50 -0500

    Add AccountAccess relation

--- app/Models/TrinityCore/Auth/AccountAccess.php
+++ app/Models/TrinityCore/Auth/AccountAccess.php
@@ -4,8 +4,27 @@ namespace App\Models\TrinityCore\Auth;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AccountAccess extends Model
 {
     use HasFactory;
+
+    protected $connection = 'tc_auth';
+    protected $table = 'account_access';
+    public $incrementing = false;
+    public $timestamps = false;
+
+    protected $casts = [
+        'AccountID' => 'int',
+        'SecurityLevel' => 'int',
+        'RealmID' => 'int'
+    ];
+
+    protected $guarded = [];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class, 'AccountID');
+    }
 }
