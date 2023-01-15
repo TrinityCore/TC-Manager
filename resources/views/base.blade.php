975c813b4d164ac6c2a615e10dbecdfcc9a2926b
Chris Deaton <dcdeaton@ncsu.edu>
2023-01-15 14:32:08 -0500

    Cleanup Remove Vite, using webpack instead

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,33 +1,19 @@
-{{--<!doctype html>--}}
-{{--<html class="dark">--}}
-{{--<head>--}}
-{{--    <meta charset="utf-8">--}}
-{{--    <meta name="viewport" content="width=device-width, initial-scale=1.0">--}}
-{{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
-{{--</head>--}}
-{{--<body>--}}
-{{--@yield('content')--}}
-{{--</div>--}}
-{{--</body>--}}
-{{--</html>--}}
-
-
-
-
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tailwind Starter Template - Night Admin Template: Tailwind Toolbox</title>
+    <title>TrinityCore Manager</title>
     <meta name="description" content="description here">
     <meta name="keywords" content="keywords,here">
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js" integrity="sha256-XF29CBwU1MWLaGEnsELogU6Y6rcc5nCkhhx89nFMIDQ=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"
+            integrity="sha256-XF29CBwU1MWLaGEnsELogU6Y6rcc5nCkhhx89nFMIDQ=" crossorigin="anonymous"></script>
 </head>
 <body class="bg-black-alt font-sans leading-normal tracking-normal">
 
