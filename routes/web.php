0d8f62a6925070e9ef1ff6528337c0b3fbfff6e8
Chris Deaton <dcdeaton@ncsu.edu>
2023-02-11 14:15:43 -0500

    Add example RBAC relations

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Models\TrinityCore\Auth\Account;
+use App\Models\TrinityCore\Auth\RbacLinkedPermission;
+use App\Models\TrinityCore\Auth\RbacPermission;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +18,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'home']);
+
+Route::get('/perms', function () {
+
+    /**
+     $granted = Account::find(5)->rbac_account_permissions()
+    ->where('granted', true)
+    ->select('permissionId')
+    ->pluck('permissionId');
+
+    $permissions = RbacPermission::whereIn(
+    'id',
+    $granted
+    )->pluck('id');
+
+    $linked = RbacLinkedPermission::whereIn('id', $permissions)
+    ->with('rbac_permission')
+    ->get();
+     */
+
+});
