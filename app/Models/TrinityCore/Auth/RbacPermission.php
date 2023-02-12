c017acaeea91a0fa790a218feff82d9fc1acf1aa
Chris Deaton <dcdeaton@ncsu.edu>
2023-02-12 13:51:01 -0500

    Php-cs-fixer fixes

--- app/Models/TrinityCore/Auth/RbacPermission.php
+++ app/Models/TrinityCore/Auth/RbacPermission.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-
 class RbacPermission extends Model
 {
     use HasFactory;
